18c7ecf2bfd561a5b88affe51b86b7a3a6983198
rahul1269-kaushal <59966780+rahul1269-kaushal@users.noreply.github.com>
2024-06-11 17:18:24 +0530

    Update urls.py

--- app_platform_testing/urls.py
+++ app_platform_testing/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app_platform_testing import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
