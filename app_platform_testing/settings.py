ca7cb46db89cb011ba531dbb1d42e0579c35d314
rahul1269-kaushal <59966780+rahul1269-kaushal@users.noreply.github.com>
2024-06-11 17:06:39 +0530

    Update settings.py

--- app_platform_testing/settings.py
+++ app_platform_testing/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split("
 # Application definition
 
 INSTALLED_APPS = [
+    'app_platform_testing',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
